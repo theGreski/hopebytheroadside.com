4f674c4b8e74c4c3a6dc6f866e9fff39b0782a35
Grzegorz Rekowski <102578051+theGreski@users.noreply.github.com>
2024-05-29 15:00:19 +0100

    debug

---
+++ public/ui.header.php
@@ -0,0 +1,25 @@
+<?php
+echo "
+<header>
+	<nav class='navbar navbar-expand-lg navbar-light bg-white fixed-top border-bottom shadow-sm'>
+		<!--<a class='navbar-brand' href='./'>
+			<img src='static/icon-96.png' width='30' height='30' class='d-none d-sm-inline-block' alt='logo'>
+			Hope By The Roadside
+		</a>-->
+		<a class='navbar-brand' href='./''>
+			<img src='static/logo__orange.svg' width='61' height='30' class='d-inline-block' alt='logo'>
+		</a>
+		<button class='navbar-toggler collapsed' type='button' data-toggle='collapse' data-target='#navbarCollapse'>
+			<span class='navbar-toggler-icon'></span>
+		</button>
+		<div id='navbarCollapse' class='navbar-collapse collapse'>
+			<ul class='navbar-nav ml-auto'>
+				<li class='nav-item py-2 py-lg-0'><a class='p-2' href='gospel.html'>The Gospel Message</a></li>
+				<li class='nav-item py-2 py-lg-0'><a class='p-2' href='audiovideo.html'>Audio &amp; Video</a></li>
+				<li class='nav-item py-2 py-lg-0'><a class='p-2' href='contact.html'>Contact Us</a></li>
+				<li class='nav-item py-2 py-lg-0'><a class='p-2' href='https://www.facebook.com/#!/groups/891612470967222/' target='_blank' rel='noopener'>Follow on Facebook</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
+";
